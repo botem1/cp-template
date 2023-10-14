0e0edb27a6a871428b2703c1ffa095e5851cdc9e
Bogdan Temnik <bogdantemnikconnect@gmail.com>
2023-10-14 13:23:54 +0200

    first

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <map>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+string yesorno(bool ans) { return (ans ? "YES" : "NO"); }
+
+void solve(){
+
+}
+
+int main(){
+	int tet;
+	cin >> tet;
+	while(tet--){
+
+	}
+
+	return 0;
+}
